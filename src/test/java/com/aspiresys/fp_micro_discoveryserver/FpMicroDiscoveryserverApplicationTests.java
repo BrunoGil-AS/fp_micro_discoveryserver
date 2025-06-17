fa2cc8d07d0ee1f06eb6a3d352c1d521067cfdfe
BrunoGilRamirez <bruno.gilram@gmail.com>
2025-06-17 13:14:03 -0600

    First commit

---
+++ src/test/java/com/aspiresys/fp_micro_discoveryserver/FpMicroDiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspiresys.fp_micro_discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpMicroDiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
